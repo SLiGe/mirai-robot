ad92abe6b8b4a10816fc0777349b7b94011b5b13
Gary <cnahzjl@gmail.com>
2020-10-29 21:48:51 +0800

    添加消息处理器

---
+++ src/main/java/cn/zjiali/robot/handler/Handler.java
@@ -0,0 +1,26 @@
+package cn.zjiali.robot.handler;
+
+import net.mamoe.mirai.message.FriendMessageEvent;
+import net.mamoe.mirai.message.GroupMessageEvent;
+
+/**
+ * @author zJiaLi
+ * @since 2020-10-29 20:53
+ */
+public interface Handler {
+
+    /**
+     * 处理群消息
+     *
+     * @param event 群消息事件
+     */
+    void handleGroupMessage(GroupMessageEvent event);
+
+    /**
+     * 处理好友消息
+     *
+     * @param event 好友消息事件
+     */
+    void handleFriendMessage(FriendMessageEvent event);
+
+}
