6ddcc49b103b6e70dba7e26075021c2502258db6
Gary <cnahzjl@gmail.com>
2020-11-01 10:48:06 +0800

    修改消息处理器

--- src/main/java/cn/zjiali/robot/handler/SignInHandler.java
+++ src/main/java/cn/zjiali/robot/handler/SignInHandler.java
@@ -1,38 +1,82 @@
 package cn.zjiali.robot.handler;
 
-import cn.zjiali.robot.constant.ServerUrl;
-import cn.zjiali.robot.util.HttpUtil;
-import cn.zjiali.robot.util.JsonUtil;
-import com.google.gson.JsonObject;
+import cn.zjiali.robot.config.PluginConfig;
+import cn.zjiali.robot.entity.response.SignInDataResponse;
+import cn.zjiali.robot.service.SignInService;
 import net.mamoe.mirai.message.FriendMessageEvent;
 import net.mamoe.mirai.message.GroupMessageEvent;
+import net.mamoe.mirai.message.data.At;
+import net.mamoe.mirai.utils.PlatformLogger;
 
-import java.util.Random;
 
 /**
  * @author zJiaLi
  * @since 2020-10-29 20:57
  */
 public class SignInHandler implements Handler {
-    @Override
-    public void handleGroupMessage(GroupMessageEvent event) {
-        long senderQQ = event.getSender().getId();
-        long groupNum = event.getGroup().getId();
-        JsonObject jsonObject = new JsonObject();
-        jsonObject.addProperty("qq", Long.toString(senderQQ));
-        jsonObject.addProperty("group", Long.toString(groupNum));
-        // 2群组消息
-        jsonObject.addProperty("msgType", 2);
-        int point = new Random(20).nextInt(60);
-        jsonObject.addProperty("integral", Integer.toString(point));
-        String signInDataJson = HttpUtil.httpPost(ServerUrl.SIGN_IN_URL, jsonObject);
-        JsonObject signInData = JsonUtil.json2obj(signInDataJson);
-        String status = signInData.get("status").getAsString();
-        //签到成功200  已经签到203
+
+    private static final SignInService signInService;
+
+    static {
+        signInService = new SignInService();
     }
 
     @Override
-    public void handleFriendMessage(FriendMessageEvent event) {
+    public void handleGroupMessage(GroupMessageEvent event) {
+        String message = event.getMessage().contentToString();
+        long senderQQ = event.getSender().getId();
+        long groupNum = event.getGroup().getId();
+        String signInMsg = getSignInMsg(message, senderQQ, groupNum, 2);
+        event.getGroup().sendMessage(new At(event.getSender()).plus(signInMsg));
+    }
 
+
+    @Override
+    public void handleFriendMessage(FriendMessageEvent event) {
+        String message = event.getMessage().contentToString();
+        long senderQQ = event.getSender().getId();
+        String signInMsg = getSignInMsg(message, senderQQ, 0, 1);
+        event.getSender().sendMessage(signInMsg);
+    }
+
+    private String getSignInMsg(String message, long senderQQ, long groupNum, int msgType) {
+        StringBuilder msgContentBuilder = new StringBuilder();
+        if (message.contains("签到")) {
+            SignInDataResponse response = signInService.doSignIn(Long.toString(senderQQ), Long.toString(groupNum), msgType);
+            if (response == null) {
+                return "签到服务异常,请联系管理员!";
+            }
+            String status = response.getStatus();
+            //签到成功200  已经签到203
+            if ("200".equals(status)) {
+                msgContentBuilder.append("签到成功！\n");
+                msgContentBuilder.append("\uD83D\uDCB8获得积分：").append(response.getPoints()).append("点\n");
+                msgContentBuilder.append("⭐本月积累签到：").append(response.getMonthDay()).append("天\n");
+                msgContentBuilder.append("\uD83D\uDCB3当前积分：").append(response.getIntegral()).append("点");
+                if ("1".equals(PluginConfig.sign_in_cur_level)) {
+                    msgContentBuilder.append("\n⭐当前等级:").append(response.getCurrentLevel());
+                }
+                if ("1".equals(PluginConfig.sign_in_day_sen)) {
+                    msgContentBuilder.append("\n⭐每日一句:").append(response.getTodayMsg());
+                }
+            } else if ("203".equals(status)) {
+                msgContentBuilder.append("你今天已经签到过了！");
+            }
+        } else if (message.contains("积分查询")) {
+            SignInDataResponse response = signInService.getSignInData(Long.toString(senderQQ), Long.toString(groupNum), 2);
+            if (response == null) {
+                return "签到服务异常,请联系管理员!";
+            }
+            msgContentBuilder.append("\uD83D\uDCB8总积分：").append(response.getIntegral()).append("点\n");
+            msgContentBuilder.append("⭐连续签到：").append(response.getMonthDay()).append("天\n");
+            msgContentBuilder.append("\uD83D\uDCB3总签到天数：").append(response.getTotalDay()).append("天");
+            if ("1".equals(PluginConfig.sign_in_cur_level)) {
+                msgContentBuilder.append("\n⭐当前等级:").append(response.getCurrentLevel());
+            }
+            if ("1".equals(PluginConfig.sign_in_day_sen)) {
+                msgContentBuilder.append("\n⭐每日一句:").append(response.getTodayMsg());
+            }
+        }
+        return msgContentBuilder.toString();
     }
 }
---
+++ src/main/java/cn/zjiali/robot/service/SignInService.java
@@ -0,0 +1,62 @@
+package cn.zjiali.robot.service;
+
+import cn.zjiali.robot.constant.ServerUrl;
+import cn.zjiali.robot.entity.response.SignInDataResponse;
+import cn.zjiali.robot.util.HttpUtil;
+import cn.zjiali.robot.util.JsonUtil;
+import com.google.gson.JsonObject;
+
+import java.util.Random;
+
+/**
+ * 签到服务类
+ *
+ * @author zJiaLi
+ * @since 2020-10-30 22:06
+ */
+public class SignInService {
+
+    public SignInDataResponse doSignIn(String qq, String group, int msgType) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("qq", qq);
+        jsonObject.addProperty("group", group);
+        // 1好友消息 2群组消息
+        jsonObject.addProperty("msgType", msgType);
+        int point = new Random(20).nextInt(60);
+        jsonObject.addProperty("integral", Integer.toString(point));
+        String signInDataJson = HttpUtil.httpPost(ServerUrl.SIGN_IN_URL, jsonObject);
+        JsonObject jsonObj = JsonUtil.json2obj(signInDataJson, JsonObject.class);
+        String status = jsonObj.get("status").getAsString();
+        if ("500".equals(status)){
+            return null;
+        }
+        if ("203".equals(status)) {
+            SignInDataResponse signInDataResponse = new SignInDataResponse();
+            signInDataResponse.setStatus("203");
+            return signInDataResponse;
+        }
+        String data = jsonObj.get("data").toString();
+        SignInDataResponse signInDataResponse = JsonUtil.json2obj(data, SignInDataResponse.class);
+        signInDataResponse.setStatus(status);
+        signInDataResponse.setPoints(point);
+        return signInDataResponse;
+    }
+
+    public SignInDataResponse getSignInData(String qq, String group, int msgType) {
+        JsonObject jsonObject = new JsonObject();
+        jsonObject.addProperty("qq",qq);
+        jsonObject.addProperty("group", group);
+        // 1好友消息 2群组消息
+        jsonObject.addProperty("msgType", msgType);
+        String signInDataJson = HttpUtil.httpPost(ServerUrl.SIGN_IN_DATA_URL, jsonObject);
+        JsonObject jsonObj = JsonUtil.json2obj(signInDataJson, JsonObject.class);
+        String status = jsonObj.get("status").getAsString();
+        if ("200".equals(status)){
+            String data = jsonObj.get("data").toString();
+            SignInDataResponse signInDataResponse = JsonUtil.json2obj(data, SignInDataResponse.class);
+            signInDataResponse.setStatus(status);
+            return signInDataResponse;
+        }
+        return null;
+    }
+}
