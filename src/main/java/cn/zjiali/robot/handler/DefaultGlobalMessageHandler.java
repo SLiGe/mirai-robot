56a428a31372d9363197642ddd24e07bb5fc733a
Gary <cnahzjl@gmail.com>
2021-09-04 21:32:32 +0800

    添加消息事件前置/后置处理方法默认实现

--- src/main/java/cn/zjiali/robot/handler/DefaultGlobalMessageHandler.java
+++ src/main/java/cn/zjiali/robot/handler/DefaultGlobalMessageHandler.java
@@ -1,12 +1,10 @@
 package cn.zjiali.robot.handler;
 
-import cn.hutool.core.collection.CollectionUtil;
-import cn.zjiali.robot.factory.HandlerFactory;
-import cn.zjiali.robot.factory.ServiceFactory;
+import cn.zjiali.robot.factory.MessageEventHandlerFactory;
 import cn.zjiali.robot.main.OutMessageConvert;
-import cn.zjiali.robot.main.interceptor.HandlerInterceptor;
 import cn.zjiali.robot.model.message.OutMessage;
-import net.mamoe.mirai.event.events.AbstractMessageEvent;
+import cn.zjiali.robot.util.ObjectUtil;
+import com.google.common.collect.Lists;
 import net.mamoe.mirai.event.events.FriendMessageEvent;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
 import net.mamoe.mirai.message.data.At;
@@ -33,63 +31,26 @@ public class DefaultGlobalMessageHandler implements GlobalMessageHandler {
         boolean preHandle = doPreHandle(isGroup ? groupMessageEvent : friendMessageEvent);
         if (!preHandle) return;
         String msg = isGroup ? groupMessageEvent.getMessage().contentToString() : friendMessageEvent.getMessage().contentToString();
-        List<MessageEventHandler> messageEventHandlerList = HandlerFactory.getInstance().getBeanList(MessageEventHandler.class);
+        List<MessageEventHandler> messageEventHandlerList = MessageEventHandlerFactory.getInstance().getBeanList(MessageEventHandler.class);
+        List<OutMessage> outMessageList = Lists.newLinkedList();
         for (MessageEventHandler messageEventHandler : messageEventHandlerList) {
             if (messageEventHandler.matchCommand(msg)) {
+                OutMessage outMessage;
                 if (isGroup) {
-                    OutMessage outMessage = messageEventHandler.handleGroupMessageEvent(groupMessageEvent);
+                    outMessage = messageEventHandler.handleGroupMessageEvent(groupMessageEvent);
                     String message = OutMessageConvert.getInstance().convert(outMessage);
-                    groupMessageEvent.getGroup().sendMessage(new At(groupMessageEvent.getSender().getId()).plus(message));
+                    if (!ObjectUtil.isNullOrEmpty(message))
+                        groupMessageEvent.getGroup().sendMessage(new At(groupMessageEvent.getSender().getId()).plus(message));
                 } else {
-                    OutMessage outMessage = messageEventHandler.handleFriendMessageEvent(friendMessageEvent);
+                    outMessage = messageEventHandler.handleFriendMessageEvent(friendMessageEvent);
                     String message = OutMessageConvert.getInstance().convert(outMessage);
-                    friendMessageEvent.getSender().sendMessage(message);
+                    if (!ObjectUtil.isNullOrEmpty(message)) friendMessageEvent.getSender().sendMessage(message);
                 }
+                outMessageList.add(outMessage);
                 if (!messageEventHandler.next()) break;
             }
         }
-        triggerAfterCompletion(isGroup ? groupMessageEvent : friendMessageEvent);
-    }
-
-    /**
-     * 调用前执行
-     *
-     * @param abstractMessageEvent 消息
-     * @return 执行结果
-     */
-    public boolean doPreHandle(AbstractMessageEvent abstractMessageEvent) {
-        boolean preHandle = false;
-        List<HandlerInterceptor> handlerInterceptors = ServiceFactory.getInstance().getBeanList(HandlerInterceptor.class);
-        if (CollectionUtil.isNotEmpty(handlerInterceptors)) {
-            for (HandlerInterceptor handlerInterceptor : handlerInterceptors) {
-                try {
-                    preHandle = handlerInterceptor.preHandle(abstractMessageEvent);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-                if (!preHandle) break;
-            }
-        }
-        return preHandle;
-    }
-
-
-    /**
-     * 完成后调用
-     *
-     * @param abstractMessageEvent 消息
-     */
-    public void triggerAfterCompletion(AbstractMessageEvent abstractMessageEvent) {
-        List<HandlerInterceptor> handlerInterceptors = ServiceFactory.getInstance().getBeanList(HandlerInterceptor.class);
-        if (CollectionUtil.isNotEmpty(handlerInterceptors)) {
-            for (HandlerInterceptor handlerInterceptor : handlerInterceptors) {
-                try {
-                    handlerInterceptor.afterCompletion(abstractMessageEvent);
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+        triggerAfterCompletion(isGroup ? groupMessageEvent : friendMessageEvent, outMessageList);
     }
 
 }
--- src/main/java/cn/zjiali/robot/handler/GlobalMessageHandler.java
+++ src/main/java/cn/zjiali/robot/handler/GlobalMessageHandler.java
@@ -1,8 +1,14 @@
 package cn.zjiali.robot.handler;
 
+import cn.hutool.core.collection.CollectionUtil;
+import cn.zjiali.robot.factory.ServiceFactory;
+import cn.zjiali.robot.main.interceptor.HandlerInterceptor;
+import cn.zjiali.robot.model.message.OutMessage;
 import net.mamoe.mirai.event.events.AbstractMessageEvent;
 import net.mamoe.mirai.event.events.FriendMessageEvent;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
+
+import java.util.List;
 
 /**
  * 消息事件总处理器
@@ -21,7 +27,21 @@ public interface GlobalMessageHandler {
      * @param abstractMessageEvent 消息事件
      * @return boolean
      */
-    boolean doPreHandle(AbstractMessageEvent abstractMessageEvent);
+   default boolean doPreHandle(AbstractMessageEvent abstractMessageEvent){
+       boolean preHandle = false;
+       List<HandlerInterceptor> handlerInterceptors = ServiceFactory.getInstance().getBeanList(HandlerInterceptor.class);
+       if (CollectionUtil.isNotEmpty(handlerInterceptors)) {
+           for (HandlerInterceptor handlerInterceptor : handlerInterceptors) {
+               try {
+                   preHandle = handlerInterceptor.preHandle(abstractMessageEvent);
+               } catch (Exception e) {
+                   e.printStackTrace();
+               }
+               if (!preHandle) break;
+           }
+       }
+       return preHandle;
+   }
 
     /**
      * 处理群组消息
@@ -41,6 +61,18 @@ public interface GlobalMessageHandler {
      * 消息事件后置处理
      *
      * @param abstractMessageEvent 消息事件
+     * @param outMessageList       输出消息列表
      */
-    void triggerAfterCompletion(AbstractMessageEvent abstractMessageEvent);
+    default void triggerAfterCompletion(AbstractMessageEvent abstractMessageEvent, List<OutMessage> outMessageList){
+        List<HandlerInterceptor> handlerInterceptors = ServiceFactory.getInstance().getBeanList(HandlerInterceptor.class);
+        if (CollectionUtil.isNotEmpty(handlerInterceptors)) {
+            for (HandlerInterceptor handlerInterceptor : handlerInterceptors) {
+                try {
+                    handlerInterceptor.afterCompletion(abstractMessageEvent, outMessageList);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
