1fb977ee13f0d69b5c8eb089e63d4fc7f02ce6fa
Gary <cnahzjl@gmail.com>
2022-06-21 17:49:56 +0800

    添加群组操作ws消息

---
+++ src/main/java/cn/zjiali/robot/model/response/ws/GroupAction.kt
@@ -0,0 +1,31 @@
+package cn.zjiali.robot.model.response.ws
+
+/**
+ *
+ * @author zJiaLi
+ * @since 2022-06-21 17:11
+ */
+class GroupAction {
+
+    /**
+     * 操作类型
+     */
+    var actionType: Int? = null
+
+    /**
+     * 禁言时间
+     */
+    var muteTime: Int? = null
+
+    /**
+     * 群号
+     */
+    var groupNumber: Long? = null
+
+    /**
+     * 成员号码
+     */
+    var memberNumber: Long? = null
+
+
+}
---
+++ src/main/java/cn/zjiali/robot/service/GroupActionService.kt
@@ -0,0 +1,9 @@
+package cn.zjiali.robot.service
+
+/**
+ *
+ * @author zJiaLi
+ * @since 2022-06-21 17:43
+ */
+class GroupActionService {
+}
