e9bfdc40de7250b662523a35a8cd86e7a657c0c6
Gary <cnahzjl@gmail.com>
2022-07-03 23:46:51 +0800

    test

---
+++ src/test/java/cn/zjiali/robot/test/config/PluginConfigTest.java
@@ -0,0 +1,26 @@
+package cn.zjiali.robot.test.config;
+
+import cn.zjiali.robot.constant.PluginCode;
+import cn.zjiali.robot.main.ApplicationBootStrap;
+import cn.zjiali.robot.main.interceptor.ReplyBlacklistHandlerInterceptor;
+import cn.zjiali.robot.manager.PluginManager;
+import cn.zjiali.robot.util.GuiceUtil;
+import org.junit.Test;
+
+import java.io.IOException;
+
+/**
+ * @author zJiaLi
+ * @since 2022-07-01 23:49
+ */
+public class PluginConfigTest {
+
+
+    @Test
+    public void testGetPluginConfig() throws IOException {
+        ApplicationBootStrap.getInstance().init();
+        PluginManager pluginManager = GuiceUtil.getBean(PluginManager.class);
+        String command = pluginManager.getConfigVal(PluginCode.ONE_SEN, "command", 859744199L, 357078415L);
+        System.out.println(command);
+    }
+}
--- src/test/java/cn/zjiali/robot/test/net/KotlinMapTest.kt
+++ src/test/java/cn/zjiali/robot/test/net/KotlinMapTest.kt
@@ -12,11 +12,22 @@ class KotlinMapTest {
     @Test
     fun testMap(): Unit {
         val queryReplyBlacklistParamMap: MutableMap<String, Any> = mutableMapOf()
-        queryReplyBlacklistParamMap["1"]= "1"
+        queryReplyBlacklistParamMap["1"] = "1"
         println(queryReplyBlacklistParamMap["1"])
 
         val songName = "点歌 我是"
         println(songName.startsWith("点歌"))
-        println(songName.replace("点歌",""))
+        println(songName.replace("点歌", ""))
+
+        val list: MutableList<String> = ArrayList()
+        list.add("one")
+        list.add("two")
+        list.add("three")
+        list.add("four")
+        list.forEach { s ->
+            println(s === "two")
+        }
+
+        println(list.filterNot { s -> s === "two" })
     }
 }
