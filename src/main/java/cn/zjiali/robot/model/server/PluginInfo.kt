ba8e04b093a0e5952cf34569bee0152268a1be43
Gary <cnahzjl@gmail.com>
2022-07-03 23:46:16 +0800

    Add activeFlag

--- src/main/java/cn/zjiali/robot/model/server/PluginInfo.kt
+++ src/main/java/cn/zjiali/robot/model/server/PluginInfo.kt
@@ -5,4 +5,4 @@ package cn.zjiali.robot.model.server
  * @author zJiaLi
  * @since 2022-06-21 16:00
  */
-data class PluginInfo(val pluginNane: String, val pluginCode: String)
+data class PluginInfo(val pluginNane: String, val pluginCode: String, val activeFlag: String)
