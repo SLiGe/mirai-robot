2216995c20c9a265e2f59e67693e59ce1b943884
Gary <cnahzjl@gmail.com>
2023-02-01 11:43:32 +0800

    读取本地配置文件

--- src/main/java/cn/zjiali/robot/main/ApplicationBootStrap.java
+++ src/main/java/cn/zjiali/robot/main/ApplicationBootStrap.java
@@ -132,9 +132,10 @@ public class ApplicationBootStrap {
      */
     private void loadAppConfig() throws IOException {
         InputStream configStream = null;
+        String systemConfigFileLocal = System.getProperty("application.config.file.local");
         // 是否启用本地配置文件
         String localConfigFileFlag = PropertiesUtil.getProperty("application.properties", "application.config.file.local");
-        if ("true".equals(localConfigFileFlag)) {
+        if ("true".equals(localConfigFileFlag) || "true".equals(systemConfigFileLocal)) {
             String configFilePath = System.getProperty("application.config.file");
             File configFile = new File(configFilePath);
             if (configFile.exists()) {
