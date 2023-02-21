5585ad80a3e8f2e4eca143ac1df10b3905c8f519
Gary <cnahzjl@gmail.com>
2023-02-21 16:02:30 +0800

    add comment

--- src/main/java/cn/zjiali/robot/constant/ConfigKey.kt
+++ src/main/java/cn/zjiali/robot/constant/ConfigKey.kt
@@ -7,8 +7,23 @@ package cn.zjiali.robot.constant
  */
 object ConfigKey {
 
+    /**
+     * 忽略关键字
+     */
     const val IGNORE_KEY_WORDS: String = "ignoreKeyWords"
+
+    /**
+     * 触发命令
+     */
     const val COMMAND: String = "command"
+
+    /**
+     * 使用模板标识
+     */
     const val TEMPLATE_FLAG: String = "templateFlag"
+
+    /**
+     * 模板内容
+     */
     const val TEMPLATE = "template"
 }
