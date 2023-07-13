39b85e85072fc66dc8ecc60cd0e37e3df1cfbe04
Gary <cnahzjl@gmail.com>
2023-07-13 10:42:48 +0800

    convert RobotApplication to kt

---
+++ src/main/java/cn/zjiali/robot/RobotApplication.kt
@@ -0,0 +1,41 @@
+package cn.zjiali.robot
+
+import cn.hutool.core.exceptions.ExceptionUtil
+import cn.zjiali.robot.main.ApplicationBootStrap
+import cn.zjiali.robot.manager.RobotManager
+import cn.zjiali.robot.util.CommonLogger
+import cn.zjiali.robot.util.GuiceUtil
+import net.mamoe.mirai.utils.LoggerAdapters.useLog4j2
+import java.util.concurrent.CountDownLatch
+
+/**
+ * 应用启动类
+ *
+ * @author zJiaLi
+ * @since 2020-10-29 11:09
+ */
+object RobotApplication {
+    @JvmField
+    val initLatch = CountDownLatch(1)
+    private val logger = CommonLogger(RobotApplication::class.java)
+    private fun init() {
+        logger.info("====初始化配置中====")
+        val startInitTime = System.currentTimeMillis()
+        try {
+            ApplicationBootStrap.getInstance().init()
+            logger.info("====初始化配置完成==== 共耗时: {} ms ", System.currentTimeMillis() - startInitTime)
+            logger.info("⭐⭐⭐⭐⭐⭐GitHub: https://github.com/SLiGe/mirai-robot ⭐⭐⭐⭐⭐⭐")
+        } catch (e: Exception) {
+            logger.error("====初始化配置出错,e: " + ExceptionUtil.stacktraceToString(e))
+        }
+    }
+
+    @JvmStatic
+    suspend fun main(args: Array<String>) {
+        useLog4j2()
+        init()
+        val robotManager = GuiceUtil.getBean(RobotManager::class.java)
+        val bot = robotManager.initBotBlocking()
+        bot.join()
+    }
+}
